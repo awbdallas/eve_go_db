6cdffd29a060f64f1c051e724d9aae0cfa3e2f70
awbdallas <Austin.W.Briggs@gmail.com>
2016-12-29 20:41:35 -0700

    Complete Rework
    Short and Sweet:
    Scrapping Everything
    Scraping eve-central is a bad idea. I'm going to try to move this to
    crest for everything. At the moment this commit is just a barebons just
    loading stuff in the db, and I'll work in thef future to start updating

--- eve.go
+++ eve.go
@@ -16,158 +16,33 @@ package main
 import (
 	"database/sql"
 	"encoding/json"
-	"encoding/xml"
 	"flag"
 	"fmt"
-	_ "github.com/mattn/go-sqlite3"
 	"io/ioutil"
-	"net/http"
 	"os"
-	"strconv"
+
+	_ "github.com/mattn/go-sqlite3"
 )
 
-/* The EveItem struct is intended to store items that are read in from
- * types.json that should be found in the same folder. This info is primarily
- * to store info into the database into the items table
- */
-type EveItem struct {
-	Volume   float32 `json:"volume"`
-	TypeID   int     `json:"typeID"`
-	GroupID  int     `json:"groupID"`
-	Market   bool    `json:"market"`
-	TypeName string  `json:"typeName"`
-}
-
-/* The Market_Items and Market_Item structs are to be used in order to hold
- * the information that we grab from our rest request that return xml. This info
- * is then stored into the database as market_info
- */
-type Market_Items struct {
-	Items []Market_Item `xml:"marketstat>type"`
-}
-
-type Market_Item struct {
-	Id          int `xml:"id,attr"`
-	SystemID    int
-	Min_sell    float32 `xml:"sell>min"`
-	Max_buy     float32 `xml:"buy>max"`
-	Volume_sell int     `xml:"sell>volume"`
-	Volume_buy  int     `xml:"buy>volume"`
-}
+var ITEM_FILE_PATH = `./data/types.json`
+var STATION_FILE_PATH = `./data/station_types.json`
+var DB_PATH = `./data/eve.db`
 
 func main() {
-	var dbpath string
-	var items_file_path string
-	var system string
-	flag.StringVar(&dbpath, "dbpath", "Default", "Path to DB")
-	flag.StringVar(&items_file_path, "typepath", "Default", "Path to JSON Files")
-	flag.StringVar(&system, "system", "30000263", "System to query")
 	flag.Parse()
-
-	if dbpath == "Default" {
-		fmt.Println("Need a Path to DB")
-		os.Exit(0)
-	}
 
 	var db *sql.DB
 
-	if _, err := os.Stat(dbpath); os.IsNotExist(err) {
-
-		if items_file_path == "Default" {
-			fmt.Println("Need a path to json file since DB doesn't exist")
-			os.Exit(0)
-		}
-
-		db = InitDB(dbpath)
+	if _, err := os.Stat(DB_PATH); os.IsNotExist(err) {
+		db = InitDB(DB_PATH)
 		defer db.Close()
 		CreateDB(db)
-		file, err := ioutil.ReadFile(items_file_path)
-
-		if err != nil {
-			fmt.Println(err.Error())
-			os.Exit(1)
-		}
-
-		var items []EveItem
-		json.Unmarshal(file, &items)
-		StoreItem(db, items)
+		PopulateItemTable(db)
+		PopulateStationTable(db)
 	} else {
-		db = InitDB(dbpath)
+		db = InitDB(DB_PATH)
 		defer db.Close()
 	}
-
-	urls := Make_URL(db, system)
-	raw_market_info := Get_Market_Info(urls)
-	parsed_market_info := Parse_Market_Info(raw_market_info)
-	Store_Market_Data(db, parsed_market_info, system)
-}
-
-/*
-* Purpose: Parse Market Information with the use of xml library
-* Parameters: info comes in as a slice of strings
-* Returns: slice of Market_Item
- */
-func Parse_Market_Info(raw_market_info []string) []Market_Item {
-	var parsed_items []Market_Item
-
-	for _, raw_items := range raw_market_info {
-		var items Market_Items
-		xml.Unmarshal([]byte(raw_items), &items)
-		for _, item := range items.Items {
-			parsed_items = append(parsed_items, item)
-		}
-	}
-	return parsed_items
-}
-
-/*
-* Purpose: Get market info by REST calls
-* Parameters: slice of strings that contain urls
-* Returns: slice of strings that contain all the info that were getting
-* by the rest calls
- */
-func Get_Market_Info(urls []string) []string {
-	var market_info []string
-
-	for _, url := range urls {
-		resp, err := http.Get(url)
-		defer resp.Body.Close()
-		if err != nil {
-			panic(err)
-		}
-
-		body, err := ioutil.ReadAll(resp.Body)
-		if err != nil {
-			panic(err)
-		}
-		market_info = append(market_info, string(body))
-	}
-
-	return market_info
-}
-
-/*
-* Purpose: Make the URLS for querying
-* Parameters: db pointer, and a value for the system to query
-* Returns: slice of strings that contain the urls
- */
-func Make_URL(db *sql.DB, system string) []string {
-	typeids := Get_Market_Items(db)
-	base_url := "http://api.eve-central.com/api/marketstat?"
-	var urls []string
-
-	url := base_url
-	for i, item := range typeids {
-		// Queries limited to 100 items per query
-		if (i%100 == 0) && (i != 0) {
-			url += ("usesystem=" + system)
-			urls = append(urls, url)
-			url = base_url
-		} else {
-			url += ("typeid=" + strconv.Itoa(item) + "&")
-		}
-	}
-	return urls
 }
 
 /*
@@ -175,7 +50,7 @@ func Make_URL(db *sql.DB, system string) []string {
 * Parameters: pointer to DB connection
 * Returns: int slice containing all the typeids
  */
-func Get_Market_Items(db *sql.DB) []int {
+func GetMarketItems(db *sql.DB) []int {
 	// SELECT all items that are on the market
 	sql_readall := `
 	SELECT TypeID FROM items
@@ -219,113 +94,161 @@ func InitDB(filepath string) *sql.DB {
 }
 
 /*
-* Purpose: Create two tables for the DB. items and market_data
-* table
+* Purpose: create db if doesn't exist
 * Returns: Nothing
 * Parameters: pointer to DB
  */
 func CreateDB(db *sql.DB) {
-	// create table if not exists
+
 	item_table := `
 	CREATE TABLE IF NOT EXISTS items(
-		TypeID INT NOT NULL PRIMARY KEY,
-		GroupID INT,
-		TypeName TEXT,
-		Volume INT,
-		Market BOOLEAN
+		typeID INT NOT NULL PRIMARY KEY,
+		groupID INT,
+		typeName TEXT,
+		volume INT,
+		market BOOLEAN
 	);
 	`
-	market_table := `
+
+	item_history_table := `
 	CREATE TABLE IF NOT EXISTS market_data(
 		id INTEGER PRIMARY KEY AUTOINCREMENT,
-		TypeID INT,
-		SystemID INT,
-		Min_sell INT,
-		Max_buy INT,
-		Volume_sell INT,
-		Volume_buy INT,
-		date_of_info TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+		typeID INT,
+		regionID INT,
+		orderCount INT,
+		lowPrice INT,
+		highPrice INT,
+		avgPrice INT,
+		volume INT,
+		date DATE
+	);
+	`
+
+	station_table := `
+	CREATE TABLE IF NOT EXISTS station(
+	  stationID INT NOT NULL PRIMARY KEY,
+	  regionID INT,
+	  solarSystemID INT,
+	  stationName TEXT
+	);	
+	`
+
+	market_orders := `
+	CREATE TABLE IF NOT EXISTS market_orders(
+	  id INTEGER PRIMARY KEY,
+	  issued DATETIME,
+	  buy BOOLEARN,
+	  price INT,
+	  volumeEntered INT,
+	  stationID INT,
+	  volume INT,
+	  range TEXT,
+	  duration INT,
+	  typeID INT
 	);
 	`
 
 	_, err := db.Exec(item_table)
+	CheckErr(err)
+	_, err = db.Exec(item_history_table)
+	CheckErr(err)
+	_, err = db.Exec(market_orders)
+	CheckErr(err)
+	_, err = db.Exec(station_table)
+	CheckErr(err)
 
-	if err != nil {
-		panic(err)
-	}
-	_, err = db.Exec(market_table)
-
-	if err != nil {
-		panic(err)
-	}
 }
 
 /*
 * Purpose: Store Items gathered from types.json into the db for the  item
 * table
 * Returns: Nothing
-* Parameters: pointer to DB and slice of EveItem struct
 * Notes: Multiply Volume by 1000 since we were having issues with storing
 * decimals
-* TODO: Optimize Insert
  */
-func StoreItem(db *sql.DB, items []EveItem) {
+func PopulateItemTable(db *sql.DB) {
+	/* The EveItem struct is intended to store items that are read in from
+	 * types.json that should be found in the same folder. This info is primarily
+	 * to store info into the database into the items table
+	 */
+
+	type EveItem struct {
+		Volume   float32 `json:"volume"`
+		TypeID   int     `json:"typeID"`
+		GroupID  int     `json:"groupID"`
+		Market   bool    `json:"market"`
+		TypeName string  `json:"typeName"`
+	}
+
+	file, err := ioutil.ReadFile(ITEM_FILE_PATH)
+	CheckErr(err)
+
+	var items []EveItem
+	json.Unmarshal(file, &items)
+
 	sql_additem := `
 	INSERT OR REPLACE INTO items(
-		TypeID,
-		GroupID,
-		TypeName,
-		Volume,
-		Market
+		typeID,
+		groupID,
+		typeName,
+		volume,
+		market
 	) values(?, ?, ?, ?, ?)
 	`
 
 	stmt, err := db.Prepare(sql_additem)
-	if err != nil {
-		panic(err)
-	}
+	CheckErr(err)
 	defer stmt.Close()
 
 	for _, item := range items {
-		_, err2 := stmt.Exec(item.TypeID, item.GroupID, item.TypeName,
+		_, err := stmt.Exec(item.TypeID, item.GroupID, item.TypeName,
 			item.Volume*1000, item.Market)
-		if err2 != nil {
-			panic(err2)
-		}
+		CheckErr(err)
+	}
+}
+
+func PopulateStationTable(db *sql.DB) {
+
+	type StationType struct {
+		StationID     int    `json:"stationID,string"`
+		RegionID      int    `json:"regionID,string"`
+		SolarSystemID int    `json:"solarSystemID,string"`
+		StationName   string `json:"stationName"`
+	}
+
+	file, err := ioutil.ReadFile(STATION_FILE_PATH)
+	CheckErr(err)
+
+	var stations []StationType
+	json.Unmarshal(file, &stations)
+
+	sql_additem := `
+	INSERT OR REPLACE INTO station(
+	  stationID,
+	  regionID,
+	  solarSystemID,
+	  stationName
+	) values(?, ?, ?, ?)
+	`
+
+	stmt, err := db.Prepare(sql_additem)
+	CheckErr(err)
+	defer stmt.Close()
+
+	for _, station := range stations {
+		_, err := stmt.Exec(station.StationID, station.RegionID,
+			station.SolarSystemID, station.StationName)
+		CheckErr(err)
 	}
 }
 
 /*
-* Purpose: Take in a slice of Market_Item and store them in the database
-* Returns: Nothing
-* Parameters: pointer to DB, slice of struct Market_Items, system to store
+* Purpose: Check err (database errors)
+* Parameters: err
  */
-func Store_Market_Data(db *sql.DB, items []Market_Item, system string) {
-	sql_additem := `
-	INSERT OR REPLACE INTO market_data(
-		TypeID,
-		SystemID,
-		Min_sell,
-		Max_buy,
-		Volume_sell,
-		Volume_buy
-	) values(?, ?, ?, ?, ?, ?)
-	`
-
-	stmt, err := db.Prepare(sql_additem)
-
+func CheckErr(err error) {
 	if err != nil {
+		fmt.Println(err.Error())
 		panic(err)
-	}
-
-	defer stmt.Close()
-
-	for _, item := range items {
-		_, err2 := stmt.Exec(item.Id, system, (item.Min_sell * 100),
-			(item.Max_buy * 100), item.Volume_sell, item.Volume_buy)
-
-		if err2 != nil {
-			panic(err2)
-		}
 	}
 }
